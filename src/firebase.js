f0fe1abd140bfffb7df7fa471e2e97c85e55c947
mabgcm <bugucam@gmail.com>
2023-03-20 22:56:50 -0400

    .env file

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDxkc_uznQHRoTI5eObaH4HC56cib8nrfA",
-    authDomain: "hicanada-673f1.firebaseapp.com",
-    projectId: "hicanada-673f1",
-    storageBucket: "hicanada-673f1.appspot.com",
-    messagingSenderId: "983004016447",
-    appId: "1:983004016447:web:ae49b31547fdd51230ad8a",
-    measurementId: "G-ZL75L9668G"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
